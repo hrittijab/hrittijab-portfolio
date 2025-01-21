7f8be19cc9d4bbc20c798704045bb555ab0a0f82
hrittijab <hrittija2001@gmail.com>
2025-01-20 21:12:50 -0700

    Spelling error fixed

--- src/pages/Volunteering.js
+++ src/pages/Volunteering.js
@@ -6,10 +6,10 @@ const Volunteering = () => {
       <h3>Volunteering</h3>
       <ul>
         <li>
-          I supported an organization in Bangladesh that focuses on the welfare of underprivileged children, where I was responsible for tutoring children in spoken English. This involved developing lesson plans, creating interactive activities, and helping the children build their confidence in speaking and understanding English. I worked with children from different backgrounds, and my role also included providing emotional support to help them overcome educational barriers.
+          I supported the Jaago Foundation which is a non profit organization in Bangladesh that focuses on the welfare of underprivileged children, where I was responsible for tutoring children in spoken English. This involved developing lesson plans, creating interactive activities, and helping the children build their confidence in speaking and understanding English. I worked with children from different backgrounds, and my role also included providing emotional support to help them overcome educational barriers.
         </li>
         <li>
-          Volunteered at events such as the Eating Disorder Awareness and Mental Health campaigns. During these events, I helped organize activities that aimed to educate the community about eating disorders and mental health awareness. These experiences helped me learn how to effectively communicate sensitive topics and support people in need.
+          Volunteered at events such as the Eating Disorder Awareness Program, Mental Health Campaign and natHack(Hackathons)
         </li>
       </ul>
     </div>
